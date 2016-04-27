badb3a21d7836c1a342f883cf696c9d313b5e96a
Filip Radelic <filip@me.com>
2016-04-27 20:09:05 +0200

    Initial Commit

---
+++ RLMIssue3489/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RLMIssue3489
+//
+//  Created by Filip Radelic on 27.04.2016..
+//  Copyright © 2016. Filip Radelic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
